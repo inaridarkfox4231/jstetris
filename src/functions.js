e076410ec8df95ca02805670098a51f7c9d37201
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-07-29 07:47:07 +0900

    不具合を修正

--- src/functions.js
+++ src/functions.js
@@ -13,7 +13,7 @@ function init(){
   for(j = 0; j < 25; j++){ Matrix[j][0] = 8; Matrix[j][11] = 8; }
   // 最下段を8にして判定に使う
   for(i = 0; i < 12; i++){ Matrix[24][i] = 8; }
-  //makeBlock();
+  makeBlock();
 }
 
 // アップデート関数
@@ -21,17 +21,18 @@ function update(){
   // PLAYの時は落とすかどうか判定する、落としたときにFREEZEに行くかどうか判定。
   if(state == PLAY){
     frame += 1;
-    if(frame > FALL_SPEED){
+    if(frame > fall_speed){
       frame = 0;
       check();  // 落とせるなら落とす、でなければFREEZEにする
     }
   }
   // FREEZEの時は必要なら行消ししてそれからブロック生成などしてPLAYに戻る
   if(state == FREEZE){
-    if(eraseLine.length > 0){
+    var length = eraseLine.length;
+    if(length > 0){
       var p = eraseLine[0];
-      for(j = 0; j < eraseLine.length; j++){ Matrix.splice(p, 1); }
-      for(j = 0; j < eraseLine.length; j++){ Matrix.unshift([8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8]); }
+      for(j = 0; j < length; j++){ Matrix.splice(p, 1); }
+      for(j = 0; j < length; j++){ Matrix.unshift([8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8]); }
       eraseLine = [];
     }
     makeBlock();
@@ -46,7 +47,7 @@ function draw(){
   for(j = 4; j < 24; j++){
     for(i = 1; i < 11; i++){
       if(Matrix[j][i] > 0){
-        drawtile(20 * (i - 1) + 120, 20 * j - 60, type);
+        drawtile(20 * (i - 1) + 70, 20 * j - 60, Matrix[j][i]);
       }
     }
   }
@@ -54,7 +55,7 @@ function draw(){
   if(state == PLAY){
     for(k = 0; k < 4; k++){
       if(ty[k] > 3){
-        drawtile(20 * (tx[k] - 1) + 120, 20 * ty[k] - 60, type);
+        drawtile(20 * (tx[k] - 1) + 70, 20 * ty[k] - 60, type);
       }
     }
   }
@@ -62,10 +63,9 @@ function draw(){
   if(state == FREEZE){
     if(frame > 0){
       frame++;
-      if((frame / 4) % 2 == 0){
+      if((frame / 8) % 2 == 0){
         for(k = 0; k < eraseLine.length; k++){
-          var p = eraseLine[k];
-          ctx.drawImage(blank, 120, p * 20 - 60);
+          ctx.drawImage(blank, 70, eraseLine[k] * 20 - 60);
         }
       }
     }
@@ -108,7 +108,7 @@ function setBlock(){
   }
   if(type < 7){
     tx[1] = tx[0] + dx[phase]; tx[2] = tx[0] - dx[phase];
-    ty[1] = ty[0] + dy[phase]; ty[2] = ty[0] - dy[phase]; return;
+    ty[1] = ty[0] + dy[phase]; ty[2] = ty[0] - dy[phase];
     if(type == 4){
       tx[3] = tx[0] + dy[phase] - dx[phase];
       ty[3] = ty[0] - dx[phase] - dy[phase]; return;
@@ -128,42 +128,11 @@ function setBlock(){
   }
 }
 
-// 回転可能かどうか調べる関数（true, falseを返す）
-function rollable(){
-  if(type == 7){ return true; }
-  var a = tx[0];
-  var b = ty[0];
-  if(type < 4){
-    var q = (phase + 1) % 2;
-    if(type == 1){
-      return Matrix[b - 1 + a][a - q] == 0 && Matrix[b + 1 - q][a + q] == 0 && Matrix[b + 2 * (1 - q)][a + 2 * q] == 0;
-    }
-    if(type == 2){
-      return Matrix[b][a + 1 - 2 * q] == 0 && Matrix[b - 1 + 2 * q][a + 1] == 0;
-    }
-    if(type == 3){
-      return Matrix[b - 1 + 2 * q][a] == 0 && Matrix[b + 1][a + 1 - 2 * q] == 0;
-    }
-  }
-  if(type < 7){
-    q = (phase + 1) % 4;  // qは次の位相
-    if(Matrix[b + dy[q]][a + dx[q]] > 0 || Matrix[b - dy[q]][a - dx[q]] > 0){ return false; }
-    if(ty == 4){
-      return Matrix[b - dx[q] - dy[q]][a + dy[q] - dx[q]] == 0;
-    }
-    if(ty == 5){
-      return Matrix[b + dx[q] - dy[q]][a - dx[q] - dy[q]] == 0;
-    }
-    if(ty == 6){
-      return Matrix[b - dx[q]][a + dy[q]] == 0;
-    }
-  }
-}
-
 // 落とせるなら落とす、落とせないならFREEZEにする
 function check(){
   for(k = 0; k < 4; k++){
     if(Matrix[ty[k] + 1][tx[k]] > 0){  // 落とせない場合（1マス下が埋まってる）
+      //console.log("freeze");
       state = FREEZE;
       write();  // 書き込み
       eraseCheck(); // 行が消えるかどうかチェック
--- src/keyevent.js
+++ src/keyevent.js
@@ -4,14 +4,14 @@ document.addEventListener("keydown", keyDownHandler, false);
 
 // 十字キーの右で右移動、左で左移動、上で回転、下で強制落下。
 function keyDownHandler(e){
-    if(e.keyCode == 39){ slide(1);
-    }else if(e.keyCode == 37){ slide(-1);
-    }else if(e.keyCode == 38){
+    if(e.keyCode == 39){　slide(1);　}
+    else if(e.keyCode == 37){ slide(-1); }
+    else if(e.keyCode == 38){
       if(rollable()){
         phase = (phase + 1) % 4;
         setBlock();
       }
-    }
+    }else if(e.keyCode == 40){ frame = fall_speed; }
 }
 
 // diffが1なら右移動、-1なら左移動
@@ -22,7 +22,40 @@ function slide(diff){
     for(k = 0; k < 4; k++){ tx[k]++; }
   }else{
     for(k = 0; k < 4; k++){ if(tx[k] <= 1){ return; }}
-    for(k = 0; k < 4; k++){ if(Matrix[ty[k]][tx[k] - 1]) > 0{ return; }}
+    for(k = 0; k < 4; k++){ if(Matrix[ty[k]][tx[k] - 1] > 0){ return; }}
     for(k = 0; k < 4; k++){ tx[k]--; }
+  }
+}
+
+// 回転可能かどうか調べる関数（true, falseを返す）
+function rollable(){
+  if(type == 7){ return true; }
+  var a = tx[0];
+  var b = ty[0];
+  if(type < 4){
+    var q = (phase + 1) % 2;
+    if(type == 1){
+      return (Matrix[b - 1 + a][a - q] == 0) && (Matrix[b + 1 - q][a + q] == 0) && (Matrix[b + 2 * (1 - q)][a + 2 * q] == 0);
+    }
+    if(type == 2){
+      return (Matrix[b][a + 1 - 2 * q] == 0) && (Matrix[b - 1 + 2 * q][a + 1] == 0);
+    }
+    if(type == 3){
+      return (Matrix[b - 1 + 2 * q][a] == 0) && (Matrix[b + 1][a + 1 - 2 * q] == 0);
+    }
+  }
+  if(type < 7){
+    // typeをtyって書いてた（信じられない）
+    var q = (phase + 1) % 4;  // qは次の位相
+    if(Matrix[b + dy[q]][a + dx[q]] > 0 || Matrix[b - dy[q]][a - dx[q]] > 0){ return false; }
+    if(type == 4){
+      return Matrix[b - dx[q] - dy[q]][a + dy[q] - dx[q]] == 0;
+    }
+    if(type == 5){
+      return Matrix[b + dx[q] - dy[q]][a - dx[q] - dy[q]] == 0;
+    }
+    if(type == 6){
+      return Matrix[b - dx[q]][a + dy[q]] == 0;
+    }
   }
 }
